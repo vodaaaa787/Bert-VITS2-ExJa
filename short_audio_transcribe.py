995a30d0e22b0e812e145c183468958a70c66664
vodaaaa787 <psyunsnow@gmail.com>
2025-04-01 23:21:47 +0800

    Update short_audio_transcribe.py

--- short_audio_transcribe.py
+++ short_audio_transcribe.py
@@ -61,7 +61,6 @@ if __name__ == "__main__":
     assert (torch.cuda.is_available()), "Please enable GPU in order to run Whisper!"
     model = whisper.load_model(args.whisper_size)
     parent_dir = config.resample_config.in_dir
-    parent_dir = parent_dir.replace("/audios","")
     speaker_names = list(os.walk(parent_dir))[0][1]
     speaker_annos = []
     total_files = sum([len(files) for r, d, files in os.walk(parent_dir)])
@@ -91,7 +90,7 @@ if __name__ == "__main__":
                 if lang not in list(lang2token.keys()):
                     print(f"{lang} not supported, ignoring\n")
                     continue
-                text = lang2token[lang] + text + lang2token[lang] + "\n"
+                text = lang2token[lang] + text + "\n"
                 speaker_annos.append(save_path + "|" + speaker + "|" + text)
                 
                 processed_files += 1
